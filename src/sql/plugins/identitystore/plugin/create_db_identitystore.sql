ee3e9eb5c330292e734fcb1e590ac6ba46bd8681
Loris Boiteux <lboiteux@yupiik.com>
2023-07-06 10:39:20 +0200

    LUT-24948 move suspicions DAO to plugin IDS

--- src/sql/plugins/identitystore/plugin/create_db_identitystore.sql
+++ src/sql/plugins/identitystore/plugin/create_db_identitystore.sql
@@ -319,3 +319,30 @@ CREATE TABLE  identitystore_identity_history
     );
 
 CREATE INDEX identitystore_identity_history_cuid ON identitystore_identity_history (customer_id);
+
+--
+-- Structure for table identitystore_quality_suspicious_identity
+--
+
+DROP TABLE IF EXISTS identitystore_quality_suspicious_identity;
+CREATE TABLE identitystore_quality_suspicious_identity (
+    id_suspicious_identity int AUTO_INCREMENT,
+    customer_id varchar(50) default '' NOT NULL,
+    id_duplicate_rule INT NOT NULL DEFAULT 0,
+    date_create timestamp default CURRENT_TIMESTAMP not null,
+    last_update_date timestamp,
+    date_delete timestamp,
+    is_deleted smallint default 0,
+    PRIMARY KEY (id_suspicious_identity)
+);
+
+
+--
+-- Structure for table identitystore_quality_suspicious_identity_excluded
+--
+
+DROP TABLE IF EXISTS identitystore_quality_suspicious_identity_excluded;
+CREATE TABLE identitystore_quality_suspicious_identity_excluded (
+    first_cuid varchar(50) default '' NOT NULL,
+    second_cuid varchar(50) default '' NOT NULL
+);
