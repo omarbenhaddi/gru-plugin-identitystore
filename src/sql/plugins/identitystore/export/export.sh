53c1c06996d2c0669b1eced92324b31df74a2cf1
Loris Boiteux <lboiteux@yupiik.com>
2023-10-24 17:52:50 +0200

    LUT-24948 - change sql export delimiter

--- src/sql/plugins/identitystore/export/export.sh
+++ src/sql/plugins/identitystore/export/export.sh
@@ -1,4 +1,4 @@
 CONN="psql -h 172.17.0.3 -U idstore -d idstore"
 QUERY="$(sed 's/;//g;/^--/ d;s/--.*//g;' export_identities.sql | tr '\n' ' ')"
 LIMITED_QUERY="$QUERY LIMIT $1"
-echo "\\copy ($LIMITED_QUERY) to 'out.csv' with CSV HEADER" | $CONN
+echo "\\copy ($LIMITED_QUERY) to 'out.csv' DELIMITER ';' CSV HEADER" | $CONN
