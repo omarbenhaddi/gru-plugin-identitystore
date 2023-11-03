af97c8cabaf91443b23918c302b3f1061997533e
Loris Boiteux <lboiteux@yupiik.com>
2023-11-03 17:36:14 +0100

    LUT-24498 - modified export for load tests

--- src/sql/plugins/identitystore/export/export.sh
+++ src/sql/plugins/identitystore/export/export.sh
@@ -1,4 +1,4 @@
-CONN="psql -h 172.17.0.3 -U idstore -d idstore"
+CONN="$1"
 QUERY="$(sed 's/;//g;/^--/ d;s/--.*//g;' export_identities.sql | tr '\n' ' ')"
-LIMITED_QUERY="$QUERY LIMIT $1"
-echo "\\copy ($LIMITED_QUERY) to 'out.csv' DELIMITER ';' CSV HEADER" | $CONN
+LIMITED_QUERY="$QUERY where i.id_identity between $2 and $3"
+echo "\\copy ($LIMITED_QUERY) to 'idstore-jdd-$2-$3.csv' DELIMITER ';' CSV HEADER" | $CONN
