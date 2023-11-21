75b77867f603846ee8cbf14bef59d690a897f14d
Loris Boiteux <lboiteux@yupiik.com>
2023-11-21 14:03:21 +0100

    LUT-24948 - remove unnecessary daemon

--- src/sql/plugins/identitystore/upgrade/update_db_identitystore-3.1.0_3.1.1.sql
+++ src/sql/plugins/identitystore/upgrade/update_db_identitystore-3.1.0_3.1.1.sql
@@ -1,3 +1,7 @@
+-- Enhance queries
 CREATE INDEX identitystore_identity_master_id ON identitystore_identity (id_master_identity);
 CREATE INDEX identitystore_quality_suspicious_identity_cuid ON identitystore_quality_suspicious_identity (customer_id);
 CREATE INDEX identitystore_identity_expiration_date ON identitystore_identity (expiration_date);
+
+-- Remove unused daemon
+DELETE FROM core_datastore WHERE entity_key IN ('core.daemon.identityDuplicatesPurgeDaemon.interval', 'core.daemon.identityDuplicatesPurgeDaemon.onStartUp');
