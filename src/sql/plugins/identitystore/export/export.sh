5da39d91dbe1e7ea50896396ed309ecdcd462a4c
Loris Boiteux <lboiteux@yupiik.com>
2023-11-28 09:56:59 +0100

    LUT-24948 - reduce sql export for load tests

--- src/sql/plugins/identitystore/export/export.sh
+++ src/sql/plugins/identitystore/export/export.sh
@@ -1,4 +1,4 @@
 CONN="$1"
 QUERY="$(sed 's/;//g;/^--/ d;s/--.*//g;' export_identities.sql | tr '\n' ' ')"
-LIMITED_QUERY="$QUERY where i.id_identity between $2 and $3"
+LIMITED_QUERY="$QUERY LIMIT $2 OFFSET $3"
 echo "\\copy ($LIMITED_QUERY) to 'idstore-jdd-$2-$3.csv' DELIMITER ';' CSV HEADER" | $CONN
--- src/sql/plugins/identitystore/export/export_identities.sql
+++ src/sql/plugins/identitystore/export/export_identities.sql
@@ -1,12 +1,21 @@
-select
-    i.customer_id as customer_id,
-    i.connection_id as connection_id,
-    family_name.attribute_value as family_name,
-    first_name.attribute_value as first_name,
-    birthdate.attribute_value as birthdate,
-    email.attribute_value as email
+select i.customer_id               as customer_id,
+       i.connection_id             as connection_id,
+       family_name.attribute_value as family_name,
+       first_name.attribute_value  as first_name,
+       birthdate.attribute_value   as birthdate,
+       email.attribute_value       as email
 from identitystore_identity i
-         left join identitystore_identity_attribute family_name on family_name.id_identity = i.id_identity and family_name.id_attribute = 3
-         left join identitystore_identity_attribute first_name on first_name.id_identity = i.id_identity and first_name.id_attribute = 4
-         left join identitystore_identity_attribute birthdate on birthdate.id_identity = i.id_identity and birthdate.id_attribute = 5
-         left join identitystore_identity_attribute email on email.id_identity = i.id_identity and email.id_attribute = 12
+         left join identitystore_identity_attribute family_name
+                   on family_name.id_identity = i.id_identity and family_name.id_attribute = 3
+         left join identitystore_identity_attribute first_name
+                   on first_name.id_identity = i.id_identity and first_name.id_attribute = 4
+         left join identitystore_identity_attribute birthdate
+                   on birthdate.id_identity = i.id_identity and birthdate.id_attribute = 5
+         left join identitystore_identity_attribute email
+                   on email.id_identity = i.id_identity and email.id_attribute = 12
+where i.customer_id is not null
+  and i.connection_id is not null
+  and family_name.attribute_value is not null
+  and first_name.attribute_value is not null
+  and birthdate.attribute_value is not null
+  and email.attribute_value is not null
