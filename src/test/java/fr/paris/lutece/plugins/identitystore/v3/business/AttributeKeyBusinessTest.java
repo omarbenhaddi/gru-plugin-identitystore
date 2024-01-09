38ae4466395ef3aac026aeeedf06f448c2061a7e
Clément Roure <croure@yupiik.com>
2024-01-09 11:17:13 +0200

    licence update

--- src/test/java/fr/paris/lutece/plugins/identitystore/v3/business/AttributeKeyBusinessTest.java
+++ src/test/java/fr/paris/lutece/plugins/identitystore/v3/business/AttributeKeyBusinessTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2002-2023, City of Paris All rights reserved.
+ * Copyright (c) 2002-2024, City of Paris All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
  *
