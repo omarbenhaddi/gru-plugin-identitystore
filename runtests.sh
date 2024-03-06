2c96f3c716dff434a969a21dcae4a20d50a981ab
sebastien leridon <sebleridon@laposte.net>
2024-03-06 17:52:57 +0100

    LUT-24948 : update library identitybusiness min version

---
+++ runtests.sh
@@ -0,0 +1,2 @@
+mvn lutece:exploded antrun:run test -Dlutece-test-postgresql   -Dlutece-antrun-db-name=LUTEST  -Dlutece-antrun-db-host=localhost:5432   -Dlutece-antrun-db-user=postgres  -Dlutece-antrun-db-password=postgres -Dtest=IdentityStoreControlsTest
+  
