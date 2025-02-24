4f7e00fec80b09eecf982a9da4b67d0c0dbf70e1
Loris Boiteux <lboiteux@yupiik.com>
2025-02-24 15:13:43 +0100

    LUT-29289 gestion des locks intégrée dans le module quality existant

--- src/sql/plugins/identitystore/upgrade/update_db_identitystore_3.1.8_3.1.9.sql
+++
@@ -1,6 +0,0 @@
---
--- Data for table core_admin_right
---
-DELETE FROM core_admin_right WHERE id_right = 'IDENTITYSTORE_LOCKS_MANAGEMENT';
-INSERT INTO core_admin_right (id_right,name,level_right,admin_url,description,is_updatable,plugin_name,id_feature_group,icon_url,documentation_url, id_order ) VALUES
-    ('IDENTITYSTORE_LOCKS_MANAGEMENT','identitystore.adminFeature.IdentityLocks.name',1,'jsp/admin/plugins/identitystore/IdentityLocks.jsp','identitystore.adminFeature.IdentityLocks.description',0,'identitystore',NULL,NULL,NULL,9);
